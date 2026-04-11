6ce2d25764a1d9167d9591f660879f66e6eb9e3d
elpdev <148717241+elpdev@users.noreply.github.com>
2026-04-11 12:02:09 -0400

    Skip processing for blocked inbound mail

--- app/services/inbound/processors/forward.rb
+++ app/services/inbound/processors/forward.rb
@@ -4,10 +4,18 @@ module Inbound
       continue_on_error!
 
       def call
+        return if blocked_message?
+
         forwarded = Outbound::AutoForwarder.forward_matching!(context.message)
         return if forwarded.empty?
 
         context.metadata["forwarded_message_ids"] = forwarded.map(&:id)
+      end
+
+      private
+
+      def blocked_message?
+        context.metadata.dig("sender_policies", "blocked_user_ids").present?
       end
     end
   end
