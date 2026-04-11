33d533e4ce8a8000bfb4e65b7ab5a8095f5d0fba
elpdev <148717241+elpdev@users.noreply.github.com>
2026-04-11 19:18:14 -0400

    Fix calendar local time grid positioning

--- spec/requests/calendars_spec.rb
+++ spec/requests/calendars_spec.rb
@@ -23,6 +23,7 @@ RSpec.describe "Calendars", type: :request do
     expect(response).to have_http_status(:ok)
     expect(response.body).to include("Week :: APR 13 - APR 19, 2026")
     expect(response.body).to include("Design Crit")
+    expect(response.body).to include("data-controller=\"calendar-time-grid\"")
     expect(response.body).to include("data-local-timestamp-format-value=\"hour\"")
     expect(response.body).to include("data-local-timestamp-format-value=\"time\"")
     expect(response.body).to include("local")
@@ -39,6 +40,7 @@ RSpec.describe "Calendars", type: :request do
     expect(response.body).to include("Day :: THU APR 16, 2026")
     expect(response.body).to include("Night Deploy")
     expect(response.body).to include("data-local-timestamp-format-value=\"time\"")
+    expect(response.body).to include("data-calendar-time-grid-target=\"segment\"")
     expect(response.body).to include("2026-04-16T00:00:00Z")
     expect(response.body).to include("2026-04-16T02:00:00Z")
   end
