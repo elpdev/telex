d8e0c7d79c96581199db9d92fb40cd061815a7f5
lbp <148717241+elpdev@users.noreply.github.com>
2026-04-11 08:47:33 -0400

    Render command palette from application layout (#63)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def command_palette_inboxes
+    return [] unless Current.user
+
+    Inbox.active.includes(:domain).sort_by { |inbox| [inbox.domain.name, inbox.address] }
+  end
+
+  def command_palette_labels
+    return [] unless Current.user
+
+    Current.user.labels.order(:name).to_a
+  end
 end
